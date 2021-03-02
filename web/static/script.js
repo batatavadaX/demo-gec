d758dcf366e1a514c52dadcad49bfa67b003697f
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-02 16:08:13 +0530

    Delete script.js

--- web/static/script.js
+++
@@ -1,6 +0,0 @@
-let toggler = document.querySelector(".toggler");
-let fullscreen = document.querySelector(".fullscreen");
-
-toggler.addEventListener('click', () => {
-    fullscreen.classList.toggle("light");
-})
