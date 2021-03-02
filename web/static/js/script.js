dd188e3bf84a7045ddaa873d8f59184bb63a2079
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-02 18:47:33 +0530

    Update script.js

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -1,6 +1,1 @@
-let toggler = document.querySelector(".toggler");
-let fullscreen = document.querySelector(".fullscreen");
-
-toggler.addEventListener('click', () => {
-    fullscreen.classList.toggle("light");
-})
+console.log("ok");
